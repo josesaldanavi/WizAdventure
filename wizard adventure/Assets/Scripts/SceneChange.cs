a103e95ab436f7ee1dcf4bb70d1a37386ea3ace3
Jose Saldaña <josesaldanavi@gmail.com>
2018-11-29 00:01:03 -0500

    scene_start terminada

---
+++ wizard adventure/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+    public string sceneName;
+    public Animator transition_canvas;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(change());
+        }
+	}
+
+    IEnumerator change()
+    {
+        transition_canvas.SetTrigger("end");
+        yield return new WaitForSeconds(1.3f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
