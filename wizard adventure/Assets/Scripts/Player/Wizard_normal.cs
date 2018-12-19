f4831bfe19575628d585a6a28c5fe893c36c5e5b
Jose Saldaña <josesaldanavi@gmail.com>
2018-12-19 15:37:46 -0500

    averiguar sobre herencia

--- wizard adventure/Assets/Scripts/Player/Wizard_normal.cs
+++ wizard adventure/Assets/Scripts/Player/Wizard_normal.cs
@@ -4,30 +4,4 @@ using UnityEngine;
 
 public class Wizard_normal : MonoBehaviour {
     public Animator wiz_normal;
-
-    public void Movimiento()
-    {
-        if (Input.GetKey(KeyCode.A))
-        {
-            if (GetComponent<SpriteRenderer>().flipX)
-            {
-                GetComponent<SpriteRenderer>().flipX = true;
-            }
-            transform.Translate(-1.5f * Time.deltaTime, 0, 0);
-            wiz_normal.SetBool("walk", true);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            if (!GetComponent<SpriteRenderer>().flipX)
-            {
-                GetComponent<SpriteRenderer>().flipX = false;
-            }
-            transform.Translate(1.5f * Time.deltaTime, 0, 0);
-            wiz_normal.SetBool("walk", true);
-        }
-        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
-        {
-            wiz_normal.SetBool("walk", false);
-        }
-    }
 }
