71332333c06354333a53bcd570147587c1e32a0c
Jose Saldaña <josesaldanavi@gmail.com>
2018-12-22 17:09:41 -0500

    animation of walk and idle  ---> finished

--- wizard adventure/Assets/Scripts/Player/Player_Controller.cs
+++ wizard adventure/Assets/Scripts/Player/Player_Controller.cs
@@ -3,7 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Controller : MonoBehaviour {
+    [Header ("WizNormal") ]
     public Animator wiz_normal;
+    public Sprite wiz_IdleN;
+    public float speedN;
+    public bool activeWN;
+
+
+    [Header("WizIce")]
+    public Animator wiz_ice;
+    public Sprite wiz_IdleI;
+    public float speedI;
+    public bool activeWI;
+
+    [Header("WizFire")]
+    public Animator wiz_fire;
+    public Sprite wiz_IdleF;
+    public float speedF;
+    public bool activeWF;
+
+
     // Use this for initialization
     void Start () {
 		
@@ -12,10 +31,16 @@ public class Player_Controller : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        Movimiento();
+        
     }
 
-    public void Movimiento()
+    private void FixedUpdate()
+    {
+        MovimientoWizNormal();
+    }
+
+
+    public void MovimientoWizNormal()
     {
         if (Input.GetKey(KeyCode.A))
         {
@@ -23,8 +48,8 @@ public class Player_Controller : MonoBehaviour {
             {
                 GetComponent<SpriteRenderer>().flipX = true;
             }
-            transform.Translate(-1.5f * Time.deltaTime, 0, 0);
-            wiz_normal.SetBool("walk", true);
+            transform.Translate(-speedN * Time.deltaTime, 0, 0);
+            wiz_normal.SetBool("walkN", true);
         }
         if (Input.GetKey(KeyCode.D))
         {
@@ -32,13 +57,17 @@ public class Player_Controller : MonoBehaviour {
             {
                 GetComponent<SpriteRenderer>().flipX = false;
             }
-            transform.Translate(1.5f * Time.deltaTime, 0, 0);
-            wiz_normal.SetBool("walk", true);
+            transform.Translate(speedN * Time.deltaTime, 0, 0);
+            wiz_normal.SetBool("walkN", true);
         }
         if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
         {
-            wiz_normal.SetBool("walk", false);
+            wiz_normal.SetBool("walkN", false);
         }
     }
 
 }
+/* hacer que las animaciones cambien dependiendo del bool que tengan, digamos que el wF esta activo y esta en la animación de idle
+ * y de ahi tiene que pasar al wI, entonces tenemos que poner una condicion que diga que si esta activo el wF o WI o WN , cuando aprete el
+ * boton de swap (cambio) que aparezca el mago elegido- --> hacer 3 botones de cambio  y que aparezca uno trasta otro dependiendo el mago que
+ estas utliziando */
--- wizard adventure/Assets/Scripts/Player/Wizard_normal.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Wizard_normal : MonoBehaviour {
-    public Animator wiz_normal;
-}
